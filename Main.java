9cb1316a06785e384ed90ba717737877e55df960
062754915DA <39984946+062754915DA@users.noreply.github.com>
2021-01-13 14:47:19 +0000

    I completed my assignment, took about 20 mins.

--- Main.java
+++ Main.java
@@ -1,15 +1,28 @@
 class Main {
   public static void main(String[] args) {
-    //Step 1: ask user for firstname
-    
-    //Step 2: ask user for surname
-    
-    //Step 3: ask user for mothers maiden name
-    
-    //Step 4: ask user for city in which they were born
-    
-    //Step 5: starwarsname = first 3 letter of surname + first 2 letters of first name + " " + first 3 letters of motehrs maiden name + first 2 letters of town you **were born in
+    String firstName = "";
+    String surname = "";
+    String mothersMaidenName = "";
+    String birthCity = "";
 
+    String starwarsname;
+    
+    //Step 1: ask user for firstname
+        firstName = Keyboard.getText("Please insert your first name.");
+    //Step 2: ask user for surname
+        surname = Keyboard.getText("Please insert your surname.");
+
+    //Step 3: ask user for mothers maiden name
+        mothersMaidenName = Keyboard.getText("Please insert your mother's maiden name.");
+    //Step 4: ask user for city in which they were born
+        birthCity = Keyboard.getText("Please insert your city of birth.");
+
+    //Step 5: starwarsname = first 3 letter of surname + first 2 letters of first name + " " + first 3 letters of motehrs maiden name + first 2 letters of town you **were born in
+    String nameSectionOne = surname.substring(0,3);
+    String nameSectionTwo = firstName.substring(0,2);
+    String nameSectionThree = mothersMaidenName.substring(0,3);
+    String nameSectionFour = birthCity.substring(0,2);  
+        starwarsname = nameSectionOne + nameSectionTwo + nameSectionThree + nameSectionFour;
     //Step 6: display "your star wars name is " + starwarsname
     System.out.println("Your Star Wars name is " + starwarsname);
   }
